5350f435033f93095bb7d693a75c683f6a43d64c
mytrylin <mytrylin@staff.1111.com.tw>
2024-10-24 19:11:35 +0800

    init codewars

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" v-model="phone">
+    <p>{{ phoneOutput }}</p>
+  </div>
+</template>
+<script setup>
+const phone = ref('')
+const splitToArray = (val) => val.split('')
+const createPhoneNumber = (val = []) => {
+  let areaCode = val.slice(0, 4).join('')
+  let beforeNumber = val.slice(4, 7).join('')
+  let afterNumbers = val.slice(7, 10).join('')
+  return `(${areaCode}) ${beforeNumber} - ${afterNumbers}`
+}
+const phoneOutput = computed(() => createPhoneNumber(splitToArray(phone.value)))
+
+</script>
+<style scoped>
+</style>
