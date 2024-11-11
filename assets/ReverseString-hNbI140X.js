d7479e8a9118880b0d76888eb717e9d2c117b62c
mytrylin <mytrylin@staff.1111.com.tw>
2024-11-11 11:26:33 +0800

    Updates

--- assets/ReverseString-hNbI140X.js
+++
@@ -1,9 +0,0 @@
-import{f as o,c as u,d as m,w as d,u as e,o as n,g as f,v,b as _,i as g,a as x,t as h,h as k}from"./index-BZ8PaDDA.js";import{_ as B}from"./Content-B6GxBFZO.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const S={key:0},R={__name:"ReverseString",setup(V){const r=o("[Algorithm] 反轉字串 (Reverse String)"),i=o("寫一個函數來反轉給定的字串"),a=o('console.log(reverseString("hello")); // "olleh"'),t=o(""),l=u(()=>t.value.split("").reverse().join("")),c=o(`
-  \`\`\`javascript
-    function solution(A) { 
-
-      return A.split('').reverse().join('') 
-
-    }
-  \`\`\`
-`);return(j,s)=>(n(),m(B,{info:{title:e(r),description:e(i),example:e(a)},source:e(c)},{implementation:d(()=>[f(_("input",{type:"text","onUpdate:modelValue":s[0]||(s[0]=p=>g(t)?t.value=p:null),maxlength:"10"},null,512),[[v,e(t)]]),e(t)?(n(),x("div",S,h(e(l)),1)):k("",!0)]),_:1},8,["info","source"]))}};export{R as default};
